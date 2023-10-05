8443e94011a5aa883565fa50560d1b4f3df9a0dc
HyeIn Park <phi52227@naver.com>
2023-10-05 16:37:14 +0900

    fix selenium settings

--- kukminParser.py
+++ kukminParser.py
@@ -17,7 +17,7 @@ from kukminIlbo.models import (
     IssueNews,
     EntertainmentsNews,
     SportsNews,
-    LifeNews
+    LifeNews,
 )
 
 # Define the sections as a dictionary
@@ -29,23 +29,32 @@ sections = {
     "prj&sid2=0086&st=all": IssueNews,  # 이슈&탐사
     "ent": EntertainmentsNews,  # 연예
     "spo": SportsNews,  # 스포츠
-    'lif': LifeNews  # 라이프
+    "lif": LifeNews,  # 라이프
 }
+
 
 def create_session():
     # Create an HTTP session for making requests
     session = requests.Session()
     # Set headers, if needed
-    session.headers.update({'User-Agent': 'Your User Agent String'})
+    session.headers.update(
+        {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+        }
+    )
     return session
+
 
 def collect_news_urls(session, section):
     news_urls = []
     response = session.get(f"https://news.kmib.co.kr/article/list.asp?sid1={section}")
     soup = bs(response.text, "html.parser")
-    for url in soup.select("#sub > div > div.NwsCon > div.nws_list > div.nws > dl > dt"):
+    for url in soup.select(
+        "#sub > div > div.NwsCon > div.nws_list > div.nws > dl > dt"
+    ):
         news_urls.append(url.find("a")["href"])
     return news_urls
+
 
 def date_to_integer(target_date):
     target_date = target_date.split()
@@ -54,23 +63,32 @@ def date_to_integer(target_date):
     result_date = date + time[0] + time[1]
     return result_date
 
+
 def crawling_articles(session, section, News):
     all_news_urls = collect_news_urls(session, section)
     for index in range(0, min(10, len(all_news_urls))):  # Process the first 10 articles
         url = all_news_urls[index]
         response = session.get("https://news.kmib.co.kr/article/" + url)
-        html_text = response.content.decode('euc-kr', 'replace').encode('utf-8', 'replace')
+        html_text = response.content.decode("euc-kr", "replace").encode(
+            "utf-8", "replace"
+        )
         html = bs(html_text, "html.parser")
-        article_title = html.select_one("#sub > div.sub_header > div > div.nwsti > h3").get_text()
+        article_title = html.select_one(
+            "#sub > div.sub_header > div > div.nwsti > h3"
+        ).get_text()
         article_title = article_title.replace("…", "… ")
         article_title = article_title.replace("…  ", "… ")
         article_title = hl.unescape(article_title)
-        article_time = html.select_one(
-            '#sub > div.sub_header > div > div.nwsti > div > div.date > span'
-        ).get_text().strip()
+        article_time = (
+            html.select_one(
+                "#sub > div.sub_header > div > div.nwsti > div > div.date > span"
+            )
+            .get_text()
+            .strip()
+        )
         article_content = str(html.select_one("#articleBody"))
         article_content = article_content.split('<div class="view_reporter">')[0]
-        article_content = article_content.split('GoodNews paper ⓒ')[0]
+        article_content = article_content.split("GoodNews paper ⓒ")[0]
         article_content = article_content.replace("<br/>", "\n")
         article_content = article_content.replace("<b>", "")
         article_content = article_content.replace("</b>", "")
@@ -78,7 +96,7 @@ def crawling_articles(session, section, News):
         article_content = re.sub(" +", " ", article_content)
         article_content = re.sub("\n{2,}", "\n\n", article_content)
         article_content = re.sub("\n {1,}", "\n", article_content)
-        article_content = article_content.replace('\n \n', '')
+        article_content = article_content.replace("\n \n", "")
         article_content = article_content.replace("  ", " ")
         article_content = article_content.replace("...", "... ")
         article_content = article_content.replace("...  ", "... ")
@@ -93,6 +111,7 @@ def crawling_articles(session, section, News):
         except Exception as e:
             print(e)
             pass
+
 
 if __name__ == "__main__":
     total_start = time.time()
--- parser.py
+++ parser.py
@@ -1,3 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import requests
 import re
@@ -5,10 +9,6 @@ from bs4 import BeautifulSoup as bs
 import os
 import django
 import html as hl
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crawlingNews.settings")
 
@@ -21,7 +21,8 @@ from news.models import (
     ItScienceNews,
 )
 
-# Define the sections as a dictionary
+total_start = time.time()
+
 sections = {
     "100": PoliticsNews,
     "101": EconomyNews,
@@ -35,11 +36,16 @@ def create_session():
     # Create an HTTP session for making requests
     session = requests.Session()
     # Set headers, if needed
-    session.headers.update({"User-Agent": "Your User Agent String"})
+    session.headers.update(
+        {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+        }
+    )
     return session
 
 
-def collect_news_urls(session, section):
+# Function to collect news titles
+def collect_news_urls():
     news_urls = []
     for num1 in range(1, 3):
         for num2 in range(1, 6):
@@ -77,43 +83,59 @@ def date_to_integer(target_date):
             time[0] = str(int(time[0]) + 12)
     time = "".join(time)
     result_date = date + time
+
     return result_date
 
 
-def crawling_articles(session, section, News, all_news_urls):
-    for index, url in enumerate(all_news_urls):
-        response = session.get(url)
-        html_text = response.text
-        html = bs(html_text, "html.parser")
-        article_title = html.select_one("#title_area > span").get_text()
-        article_title = article_title.replace("...", "... ")
-        article_title = article_title.replace("...  ", "... ")
-        article_title = hl.unescape(article_title)
-        article_time = html.select_one(
-            "#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span"
-        ).get_text()
-        article_content = str(html.select_one("#dic_area"))
-        article_content = article_content.replace("<br/>", "\n")
-        article_content = re.sub("<.+?>", "", article_content).strip()
-        article_content = re.sub(" +", " ", article_content)
-        article_content = re.sub("\n{2,}", "\n\n", article_content)
-        article_content = article_content.replace("  ", " ")
-        article_content = article_content.replace("...", "... ")
-        article_content = article_content.replace("...  ", "... ")
-        article_content = hl.unescape(article_content)
-        try:
-            News(
-                title=article_title,
-                posting_date=article_time,
-                content=article_content,
-                order=date_to_integer(article_time),
-            ).save()
-        except Exception as e:
-            print(e)
-            pass
+def crawling_article(url, index, News, session):
+    response = requests.get(url)
+
+    html_text = response.text
+
+    html = bs(html_text, "html.parser")
+
+    article_title = html.select_one("#title_area > span").get_text()
+    article_title = article_title.replace("...", "... ")
+    article_title = article_title.replace("...  ", "... ")
+    article_title = hl.unescape(article_title)
+
+    article_time = html.select_one(
+        "#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span"
+    ).get_text()
+
+    article_content = str(html.select_one("#dic_area"))
+    article_content = article_content.replace("<br/>", "\n")
+    article_content = re.sub("<.+?>", "", article_content).strip()
+    article_content = re.sub(" +", " ", article_content)
+    article_content = re.sub("\n{2,}", "\n\n", article_content)
+    # article_content = article_content.replace(" \n", "\n")
+    # article_content = article_content.replace("\n", "\n ")
+    # article_content = article_content.replace("\n \n", "\n\n")
+    article_content = article_content.replace("  ", " ")
+    article_content = article_content.replace("...", "... ")
+    article_content = article_content.replace("...  ", "... ")
+    article_content = hl.unescape(article_content)
+    # article_content = article_content.replace("&lt;", "<")
+    # article_content = article_content.replace("&gt;", ">")
+
+    try:
+        News(
+            title=article_title,
+            posting_date=article_time,
+            content=article_content,
+            order=date_to_integer(article_time),
+        ).save()
+    except Exception as e:
+        print(e)
+        pass
 
 
-def process_section(section, News, session):
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+for section, News in sections.items():
     start = time.time()
     driver = webdriver.Chrome(
         executable_path="/root/CrawlingApi/chromedriver-linux64/chromedriver",
@@ -122,33 +144,29 @@ def process_section(section, News, session):
     driver.get(
         f"https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1={section}"
     )
+
     wait = WebDriverWait(driver, 3)
     time.sleep(0.5)
 
     # Collect news titles before and after clicking the pagination link
     all_news_urls = []
-    all_news_urls += collect_news_urls(session, section)
+    all_news_urls += collect_news_urls()
+
+    driver.find_element(By.CSS_SELECTOR, "#paging > a:nth-child(2)").click()
+    time.sleep(0.5)
+
+    # Collect news titles again after clicking the pagination link
+    all_news_urls += collect_news_urls()
 
     driver.quit()
 
-    # Process the first 10 articles
-    all_news_urls = all_news_urls[:10]
-    crawling_articles(session, section, News, all_news_urls)
+    session = create_session()
+    for index, url in enumerate(all_news_urls):
+        crawling_article(url, index, News, session)
 
     end = time.time()
     print(f"section({section}) 응답시간 : {end - start : .5f} sec")
 
 
-if __name__ == "__main__":
-    total_start = time.time()
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    session = create_session()
-
-    for section, News in sections.items():
-        process_section(section, News, session)
-
-    total_end = time.time()
-    print(f"총 응답시간 : {total_end - total_start : .5f} sec")
+total_end = time.time()
+print(f"총 응답시간 : {total_end - total_start : .5f} sec")
